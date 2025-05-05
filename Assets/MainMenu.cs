cbbe305e4ed7d36d78f279a030f737c41ecad4ff
Stephenasante21 <stephen.asante@student.ehb.be>
2025-05-06 00:52:15 +0200

    initial push + menu
    - Setup Unity 6 project structure
    - Added Main Menu UI with PLAY, SETTINGS, QUIT buttons
    - Added Settings Panel with labels and functioning Back button
    - Integrated Gotham font and Anansi-themed border visuals
    - Mobile-ready canvas with proper scaling

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/SettingsManager.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class SettingsManager : MonoBehaviour
+{
+
+
+}
